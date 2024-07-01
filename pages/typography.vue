8f4305f2bda545d9d39fbd23dcb5d61de084622c
Hans <hans.doenitz@dasei.eu>
2024-07-01 17:55:34 +0200

    integrate nuxt.studio

---
+++ pages/typography.vue
@@ -0,0 +1,50 @@
+<template>
+  <LayoutHeader />
+  <div class="container px-4 md:px-8 py-6">
+    <h1>
+      <span style="float: left; font-size: 60px; line-height: 0.85em; font-weight: bold; margin-right: 7px;">
+        A1
+      </span>
+      <span class="text-base">
+        Einführung in die Kreisanimation
+      </span>
+      <br>
+      <span style="padding-bottom: 10px;" class="text-2xl">
+        Am Anfang war der Kreis
+      </span>
+    </h1>
+
+    <div>
+      <p>
+        ein in 2022-2023 umgesetzter Design-Prozess hat die verschiedensten Anforderungen und Use-Cases ermittelt und dazu mehrere Design-Vorlagen entwickelt
+      </p>
+      <p>
+        ausgehend von Javascript-Component-Frameworks und Obsidian-Themes werden diese Design-Vorlagen weiterentwickelt und angepasst mit dem Ziel einer hohen technischen wie inhaltlichen Qualität
+      </p>
+    </div>
+
+    <div class="mt-4">
+      <h1>
+        <span style="float: left; line-height: 0.85em;" class="text-6xl pr-2">A1
+        </span>
+        <span class="text-base mt-4">Einführung in die Kreisanimation </span>
+        <span class="opacity-0"> | </span>
+        <span style="display: block; padding-bottom: 10px;" class="text-2xl">Am Anfang war der Kreis</span>
+      </h1>
+    </div>
+
+    <div>
+      <p>
+        ein in 2022-2023 umgesetzter Design-Prozess hat die verschiedensten Anforderungen und Use-Cases ermittelt und dazu mehrere Design-Vorlagen entwickelt
+      </p>
+      <p>
+        ausgehend von Javascript-Component-Frameworks und Obsidian-Themes werden diese Design-Vorlagen weiterentwickelt und angepasst mit dem Ziel einer hohen technischen wie inhaltlichen Qualität
+      </p>
+    </div>
+  </div>
+  <LayoutFooter />
+</template>
+
+<script setup lang="ts">
+
+</script>
